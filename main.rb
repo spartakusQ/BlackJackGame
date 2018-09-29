a822d43ff57c3a51b61bf5b718fd068a8926cfe6
Unknown <spartakus1688@gmail.com>
2018-09-29 22:24:57 +0300

    Add method to start game

--- main.rb
+++ main.rb
@@ -3,4 +3,14 @@ require_relative 'player'
 require_relative 'deck'
 # class to manage the game
 class MainInterface
+  def menu
+    puts 'Напишите своё имя, если хотите сыграть: '
+    name = gets.chomp.to_s
+    puts "Добрый вечер #{name}"
+    exit
+  end
+end
+
+def method_name
+  "#{}"
 end
