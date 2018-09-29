50c8d1891bf120fe6d0e8222d7fb1d7f493817d9
Unknown <spartakus1688@gmail.com>
2018-09-30 01:15:11 +0300

    Add method constructor for deck & method build deck

--- deck.rb
+++ deck.rb
@@ -1,3 +1,18 @@
 # class for controlling the mechanics of card distribution
 class Deck
+  attr_accessor :cards, :deck
+
+  def initialize
+    @cards = Deck.build_deck
+  end
+  
+  def self.build_deck
+    @cards = []
+    Card::SHIRTS.each do |shirt|
+      Card::CARDS.each do |card|
+        @cards << Card.new(rank, suit)
+      end
+    end
+    @cards.shuffle!
+  end
 end
