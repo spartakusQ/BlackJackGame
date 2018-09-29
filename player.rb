09c4204ea6d0b8b823727d6354e3ce88215dbbb1
Unknown <spartakus1688@gmail.com>
2018-09-29 22:44:14 +0300

    Add method constructor for player

--- player.rb
+++ player.rb
@@ -1,3 +1,12 @@
 # class for controlling the mechanics of the player
 class Player
+  attr_accessor :hand, :cards, :coin
+
+  def initialize(name)
+    @name = name
+    @coin = 100
+    @cards = Deck.new
+    @hand = []
+  end
+  
 end
