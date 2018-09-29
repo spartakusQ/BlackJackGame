0b295b22f7bb5d5ebb4be52220cf8440f4380df6
Unknown <spartakus1688@gmail.com>
2018-09-30 01:02:02 +0300

    Add method constructor for dealer & human

--- dealer.rb
+++ dealer.rb
@@ -1,3 +1,7 @@
+require_relative 'player'
 # class for controlling the mechanics of the dealer
-class Dealer
+class Dealer < Player
+  def initialize
+    super
+  end
 end
--- human.rb
+++ human.rb
@@ -1,6 +1,7 @@
+require_relative 'player'
 # class for controlling the mechanics of the human player
 class Human < Player
   def initialize
-    
+    super
   end
 end
