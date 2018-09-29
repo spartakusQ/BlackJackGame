3a74df20ff0ffdd13a17c409463e14b565ba1b05
Unknown <spartakus1688@gmail.com>
2018-09-29 22:39:49 +0300

    Metod for counting cards

--- card.rb
+++ card.rb
@@ -1,11 +1,19 @@
 # class for managing a deck of cards
 class Card
-  attr_reader :card, :shirt,
+  attr_reader :card, :shirt, :rating
   CARDS = %w[2 3 4 5 6 7 8 9 10 J Q K A]
   SHIRTS = ['♠', '♣', '♦', '♥']
 
   def initialize(card, shirt)
-    @card = rank
+    @card = card
     @shirt = shirt
   end
+
+  def rating
+    return @rank if %w[2 3 4 5 6 7 8 9 10].include?(rank)
+    return 10 if %w[J Q K].include?(rank)
+    return 1 if rank == 'A'
+    @value
+  end
+  
 end
--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require_relative 'dealer'
 require_relative 'player'
 require_relative 'deck'
+require_relative 'card'
 # class to manage the game
 class MainInterface
   def menu
